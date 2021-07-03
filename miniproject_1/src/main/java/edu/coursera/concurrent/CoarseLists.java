7ba3c65cab9402a94ce10ae2233fb0a9e94918da
gaomingt <gaomingt@andrew.cmu.edu>
2021-07-03 21:38:42 +0800

    week1

---
+++ miniproject_1/src/main/java/edu/coursera/concurrent/CoarseLists.java
@@ -0,0 +1,199 @@
+package edu.coursera.concurrent;
+
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * Wrapper class for two lock-based concurrent list implementations.
+ */
+public final class CoarseLists {
+    /**
+     * An implementation of the ListSet interface that uses Java locks to
+     * protect against concurrent accesses.
+     *
+     * TODO Implement the add, remove, and contains methods below to support
+     * correct, concurrent access to this list. Use a Java ReentrantLock object
+     * to protect against those concurrent accesses. You may refer to
+     * SyncList.java for help understanding the list management logic, and for
+     * guidance in understanding where to place lock-based synchronization.
+     */
+    public static final class CoarseList extends ListSet {
+        private ReentrantLock lock;
+
+        public CoarseList() {
+            super();
+            lock = new ReentrantLock();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        boolean add(final Integer object) {
+            try {
+                lock.lock();
+
+                Entry pred = this.head;
+                Entry curr = pred.next;
+
+                while (curr.object.compareTo(object) < 0) {
+                    pred = curr;
+                    curr = curr.next;
+                }
+
+                if (object.equals(curr.object)) {
+                    return false;
+                } else {
+                    final Entry entry = new Entry(object);
+                    entry.next = curr;
+                    pred.next = entry;
+                    return true;
+                }
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        boolean remove(final Integer object) {
+
+            try {
+                lock.lock();
+
+                Entry pred = this.head;
+                Entry curr = pred.next;
+
+                while (curr.object.compareTo(object) < 0) {
+                    pred = curr;
+                    curr = curr.next;
+                }
+
+                if (object.equals(curr.object)) {
+                    pred.next = curr.next;
+                    return true;
+                } else {
+                    return false;
+                }
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        boolean contains(final Integer object) {
+            try {
+                lock.lock();
+                Entry pred = this.head;
+                Entry curr = pred.next;
+
+                while (curr.object.compareTo(object) < 0) {
+                    pred = curr;
+                    curr = curr.next;
+                }
+                return object.equals(curr.object);
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    /**
+     * An implementation of the ListSet interface that uses Java read-write
+     * locks to protect against concurrent accesses.
+     *
+     * TODO Implement the add, remove, and contains methods below to support
+     * correct, concurrent access to this list. Use a Java
+     * ReentrantReadWriteLock object to protect against those concurrent
+     * accesses. You may refer to SyncList.java for help understanding the list
+     * management logic, and for guidance in understanding where to place
+     * lock-based synchronization.
+     */
+    public static final class RWCoarseList extends ListSet {
+        private ReentrantReadWriteLock rwLock;
+
+        public RWCoarseList() {
+            super();
+            rwLock = new ReentrantReadWriteLock();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        boolean add(final Integer object) {
+            try {
+                rwLock.writeLock().lock();
+                Entry pred = this.head;
+                Entry curr = pred.next;
+
+                while (curr.object.compareTo(object) < 0) {
+                    pred = curr;
+                    curr = curr.next;
+                }
+
+                if (object.equals(curr.object)) {
+                    return false;
+                } else {
+                    final Entry entry = new Entry(object);
+                    entry.next = curr;
+                    pred.next = entry;
+                    return true;
+                }
+            } finally {
+                rwLock.writeLock().unlock();
+            }
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        boolean remove(final Integer object) {
+            try {
+                rwLock.writeLock().lock();
+                Entry pred = this.head;
+                Entry curr = pred.next;
+
+                while (curr.object.compareTo(object) < 0) {
+                    pred = curr;
+                    curr = curr.next;
+                }
+
+                if (object.equals(curr.object)) {
+                    pred.next = curr.next;
+                    return true;
+                } else {
+                    return false;
+                }
+            } finally {
+                rwLock.writeLock().unlock();
+            }
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        boolean contains(final Integer object) {
+            try {
+                rwLock.readLock().lock();
+                Entry pred = this.head;
+                Entry curr = pred.next;
+
+                while (curr.object.compareTo(object) < 0) {
+                    pred = curr;
+                    curr = curr.next;
+                }
+                return object.equals(curr.object);
+            } finally {
+                rwLock.readLock().unlock();
+            }
+        }
+    }
+}
